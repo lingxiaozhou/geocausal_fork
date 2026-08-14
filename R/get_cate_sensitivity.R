169e1ea91cc6b13d6ea376fcf483845daa7a3c2f
Lingxiao Zhou <zhou.l@ufl.edu>
2026-08-14 00:21:47 -0400

    Update get_cate_sensitivity.R

--- R/get_cate_sensitivity.R
+++ R/get_cate_sensitivity.R
@@ -42,7 +42,7 @@
 get_cate_sensitivity <- function(obs, cf1, cf2, treat, pixel_count_out,lag, trunc_level=0.95, time_after=TRUE,entire_window = NULL,
                                  em = NULL,E_mat = NULL,
                                  nbase = 6, spline_type = "ns",intercept = TRUE,
-                                 eval_values = NULL, eval_mat = NULL,test_beta = NULL,save_weights = TRUE,gamma_vals = seq(1.05,1.2,0.05),...) {
+                                 eval_values = NULL, eval_mat = NULL,save_weights = TRUE,gamma_vals = seq(1.05,1.2,0.05),...) {
   # pixel_ratio <- 8451/(128*128)
   chisq_stat <- NULL
   p.value <- NULL
@@ -73,7 +73,7 @@ get_cate_sensitivity <- function(obs, cf1, cf2, treat, pixel_count_out,lag, trun
     nbase <- ncol(E_mat)+intercept
   }
   
-
+  
   
   #-------------------------get the weighted surfaces------------------------------------
   message("Get weighted surfaces... \n")
@@ -194,102 +194,410 @@ get_cate_sensitivity <- function(obs, cf1, cf2, treat, pixel_count_out,lag, trun
   #---------------------------sensitivity analysis-----------------------
   message("Perform sensitivity analysis...\n")
   
-  beta1 <- beta_arr[,1:p]
-  beta2 <- beta_arr[,(p+1):(2*p)]
+  beta1 <- beta_arr[, 1:p]
+  beta2 <- beta_arr[, (p + 1):(2 * p)]
   
-  npoints <- nrow(eval_mat)
-  D_mat <- diag(time_points)
-  bounds <- array(NA,c(2,2,npoints,length(gamma_vals)))
-  dimnames(bounds) <- list(c("1","2"),c("min","max"),eval_values,gamma_vals)
+  npoints <- nrow(eval_mat) 
+  npoints_beta <- p-intercept
   
-  lb_array <- array(NA,c(npoints,length(gamma_vals)))
-  ub_array <- array(NA,c(npoints,length(gamma_vals)))
+  # store results
+  lambda_interval <- array(NA, c(npoints, 2, length(gamma_vals)))
+  dimnames(lambda_interval) <- list(eval_values, c("lambda_lo", "lambda_hi"), gamma_vals)
   
-  for (gg in 1 : length(gamma_vals)) {
+  best_lambda <- array(NA, c(npoints, length(gamma_vals)))
+  dimnames(best_lambda) <- list(eval_values, gamma_vals)
+  
+  best_slack <- array(NA, c(npoints, length(gamma_vals)))
+  dimnames(best_slack) <- list(eval_values, gamma_vals)
+  
+  zero_attainable <- array(NA, c(npoints, length(gamma_vals)))
+  dimnames(zero_attainable) <- list(eval_values, gamma_vals)
+  
+  lambda_interval_beta <- array(NA, c(npoints_beta, 2, length(gamma_vals)))
+  dimnames(lambda_interval_beta) <- list(colMeans(beta2-beta1)[-1], c("lambda_lo", "lambda_hi"), gamma_vals)
+  
+  best_lambda_beta <- array(NA, c(npoints_beta, length(gamma_vals)))
+  dimnames(best_lambda_beta) <- list(colMeans(beta2-beta1)[-1], gamma_vals)
+  
+  best_slack_beta <- array(NA, c(npoints_beta, length(gamma_vals)))
+  dimnames(best_slack_beta) <- list(colMeans(beta2-beta1)[-1], gamma_vals)
+  
+  zero_attainable_beta <- array(NA, c(npoints_beta, length(gamma_vals)))
+  dimnames(zero_attainable_beta) <- list(colMeans(beta2-beta1)[-1], gamma_vals)
+  
+  # optional: stop once overall zero-attainability occurs
+  first_gamma_zero_attainable <- NA
+  first_gamma_zero_attainable_beta <- NA
+  
+  #------------------------------------------------------------
+  # helper: single-ratio min/max by Charnes-Cooper LP
+  #------------------------------------------------------------
+  solve_ratio_bounds <- function(obj_num, obj_den, lower_rho, upper_rho) {
+    Tlen <- length(obj_num)
+    D_mat <- diag(Tlen)
     
-    this_gamma <- gamma_vals[gg]
-    cat("Start gamma:",this_gamma,"\n")
-    for (index in 1:2) {
-      for (point in 1:npoints) {
-        
-        
-        # Starting with the linear fractional problem:
-        obj_num <- c(get(paste0("beta",index))%*%eval_mat[point,])
-        obj_den <- weights[index,]/mean(weights[index,])
-        mat <- rbind(D_mat, - D_mat)
-        dir <- rep('<=', 2 * time_points)
-        rhs <- c(rep(this_gamma ^ lag, time_points), rep(- (1 / this_gamma) ^ lag, time_points))
-        
-        # Creating the transformed linear problem:
-        new_obj <- c(obj_num, 0)
-        
-        new_mat <- cbind(mat, - rhs)
-        new_dir <- rep('<=', 2 * time_points)
-        new_rhs <- rep(0, 2 * time_points)
-        
-        new_mat <- rbind(new_mat, c(obj_den, 0))
-        new_dir <- c(new_dir, '==')
-        new_rhs <- c(new_rhs, 1)
-        
-        new_mat <- rbind(new_mat, c(rep(0, ncol(new_mat) - 1), 1))
-        new_dir <- c(new_dir, '>=')
-        new_rhs <- c(new_rhs, 0)
-        
-        lp_sol2_max <- Rglpk_solve_LP(obj = new_obj, new_mat, new_dir, new_rhs, max = TRUE, verbose = FALSE)
-        lp_sol2_min <- Rglpk_solve_LP(obj = new_obj, new_mat, new_dir, new_rhs, max = FALSE, verbose = FALSE)
-        
-        bounds[index,1,point,gg] <- lp_sol2_min$optimum
-        bounds[index,2,point,gg] <- lp_sol2_max$optimum
-      }
-    }
-    lb_array[,gg] <- sapply(1:npoints, function(x) bounds[2,1,x,gg]-bounds[1,2,x,gg])
-    ub_array[,gg] <- sapply(1:npoints, function(x) bounds[2,2,x,gg]-bounds[1,1,x,gg])
+    mat <- rbind(D_mat, -D_mat)
+    dir <- rep("<=", 2 * Tlen)
+    rhs <- c(upper_rho, -lower_rho)
+    
+    new_obj <- c(obj_num, 0)
+    
+    new_mat <- cbind(mat, -rhs)
+    new_dir <- rep("<=", 2 * Tlen)
+    new_rhs <- rep(0, 2 * Tlen)
+    
+    new_mat <- rbind(new_mat, c(obj_den, 0))
+    new_dir <- c(new_dir, "==")
+    new_rhs <- c(new_rhs, 1)
+    
+    new_mat <- rbind(new_mat, c(rep(0, ncol(new_mat) - 1), 1))
+    new_dir <- c(new_dir, ">=")
+    new_rhs <- c(new_rhs, 0)
+    
+    lp_max <- Rglpk_solve_LP(
+      obj = new_obj, mat = new_mat, dir = new_dir, rhs = new_rhs,
+      max = TRUE, verbose = FALSE
+    )
+    lp_min <- Rglpk_solve_LP(
+      obj = new_obj, mat = new_mat, dir = new_dir, rhs = new_rhs,
+      max = FALSE, verbose = FALSE
+    )
+    
+    c(min = lp_min$optimum, max = lp_max$optimum)
   }
   
-  
-  
-  robust_gamma <- function(lb_array, ub_array, gamma_vals) {
+  #------------------------------------------------------------
+  # helper: slack LP for fixed lambda
+  #------------------------------------------------------------
+  solve_lambda_slack <- function(lambda, A, B, C, D, lower_rho, upper_rho, eps_denom = 1e-8) {
+    Tlen <- length(A)
     
-    n_x <- nrow(lb_array)
-    n_gamma <- length(gamma_vals)
+    # variables: rho_1,...,rho_T,s
+    obj <- c(rep(0, Tlen), 1)
     
-    # ---------- overall ----------
-    robust_each_gamma <- sapply(seq_len(n_gamma), function(j) {
-      all((lb_array[, j] > 0) | (ub_array[, j] < 0))
-    })
+    v1 <- A - lambda * B
+    v2 <- C - lambda * D
     
-    overall_largest_gamma <- if (any(robust_each_gamma)) {
-      gamma_vals[max(which(robust_each_gamma))]
-    } else {
-      NA
+    mat <- rbind(
+      c(v1, -1),
+      c(-v1, -1),
+      c(v2, -1),
+      c(-v2, -1),
+      cbind(diag(Tlen), rep(0, Tlen)),
+      cbind(-diag(Tlen), rep(0, Tlen)),
+      c(B, 0),
+      c(D, 0),
+      c(rep(0, Tlen), 1)
+    )
+    
+    dir <- c(
+      "<=", "<=", "<=", "<=",
+      rep("<=", Tlen),
+      rep("<=", Tlen),
+      ">=", ">=",
+      ">="
+    )
+    
+    rhs <- c(
+      0, 0, 0, 0,
+      upper_rho,
+      -lower_rho,
+      eps_denom, eps_denom,
+      0
+    )
+    
+    sol <- Rglpk_solve_LP(
+      obj = obj, mat = mat, dir = dir, rhs = rhs,
+      max = FALSE, verbose = FALSE
+    )
+    
+    # if solver does not end properly, return NA
+    if (sol$status != 0) {
+      return(list(
+        status = sol$status,
+        optimum = Inf,
+        solution = NULL,
+        rho = NULL,
+        s = Inf,
+        residual1 = NA,
+        residual2 = NA,
+        max_residual = NA
+      ))
     }
     
-    # ---------- pointwise (per x) ----------
-    pointwise_largest_gamma <- sapply(seq_len(n_x), function(i) {
-      
-      robust_i <- (lb_array[i, ] > 0) | (ub_array[i, ] < 0)
-      
-      if (any(robust_i)) {
-        gamma_vals[max(which(robust_i))]
-      } else {
-        NA
-      }
-    })
+    # get rho and s
+    rho_sol <- sol$solution[1:Tlen]
+    s_sol <- sol$solution[Tlen + 1]
     
-    # ---------- output ----------
+    # check residual
+    residual1 <- sum((A - lambda * B) * rho_sol)
+    residual2 <- sum((C - lambda * D) * rho_sol)
+    max_residual <- max(abs(residual1), abs(residual2))
+    
     list(
-      overall_largest_gamma = overall_largest_gamma,
-      pointwise_largest_gamma = pointwise_largest_gamma
+      status = sol$status,
+      optimum = sol$optimum,
+      solution = sol$solution,
+      rho = rho_sol,
+      s = s_sol,
+      residual1 = residual1,
+      residual2 = residual2,
+      max_residual = max_residual
     )
   }
   
-  robust_gamma <- robust_gamma(lb_array, ub_array, gamma_vals)
+  #------------------------------------------------------------
+  # helper: adaptive search over lambda
+  #------------------------------------------------------------
+  adaptive_lambda_search <- function(lambda_lo, lambda_hi,
+                                     A, B, C, D,
+                                     lower_rho, upper_rho,
+                                     tol_slack = 1e-6,
+                                     grid_init = 15,
+                                     max_refine = 5) {
+    if (lambda_lo > lambda_hi) {
+      return(list(attainable = FALSE, best_lambda = NA, best_slack = Inf))
+    }
+    
+    current_lo <- lambda_lo
+    current_hi <- lambda_hi
+    best_lambda <- NA
+    best_slack <- Inf
+    
+    for (iter in seq_len(max_refine)) {
+      lambda_grid <- seq(current_lo, current_hi, length.out = grid_init)
+      slack_vals <- rep(NA_real_, length(lambda_grid))
+      
+      best_sol <- NULL
+      
+      for (j in seq_along(lambda_grid)) {
+        sol <- solve_lambda_slack(
+          lambda = lambda_grid[j],
+          A = A, B = B, C = C, D = D,
+          lower_rho = lower_rho, upper_rho = upper_rho
+        )
+        slack_vals[j] <- sol$optimum
+      }
+      
+      j_best <- which.min(slack_vals)
+      
+      if (slack_vals[j_best] < best_slack) {
+        best_slack <- slack_vals[j_best]
+        best_lambda <- lambda_grid[j_best]
+        
+        best_sol <- solve_lambda_slack(
+          lambda = best_lambda,
+          A = A, B = B, C = C, D = D,
+          lower_rho = lower_rho, upper_rho = upper_rho
+        )
+      }
+      
+      if (best_slack <= tol_slack) {
+        return(list(
+          attainable = TRUE,
+          best_lambda = best_lambda,
+          best_slack = best_slack,
+          residual1 = best_sol$residual1,
+          residual2 = best_sol$residual2,
+          max_residual = best_sol$max_residual,
+          rho = best_sol$rho,
+          status = best_sol$status
+        ))
+      }
+      
+      left_idx <- max(1, j_best - 1)
+      right_idx <- min(length(lambda_grid), j_best + 1)
+      
+      current_lo <- lambda_grid[left_idx]
+      current_hi <- lambda_grid[right_idx]
+      
+      if ((current_hi - current_lo) < 1e-10) break
+    }
+    
+    list(
+      attainable = (best_slack <= tol_slack),
+      best_lambda = best_lambda,
+      best_slack = best_slack
+    )
+  }
   
-  res <- list(bounds = bounds,
-              lb_array = lb_array,
-              ub_array = ub_array,
-              robust_gamma = robust_gamma)
+  #------------------------------------------------------------
+  # main gamma loop for the CATE
+  #------------------------------------------------------------
+  for (gg in seq_along(gamma_vals)) {
+    
+    this_gamma <- gamma_vals[gg]
+    cat("Start gamma:", this_gamma, "\n")
+    
+    lower_rho <- rep((1 / this_gamma)^lag, time_points)
+    upper_rho <- rep(this_gamma^lag, time_points)
+    
+    point_attainable <- logical(npoints)
+    
+    for (point in seq_len(npoints)) {
+      
+      # ratio 1
+      A <- c(beta1 %*% eval_mat[point, ])
+      B <- weights[1, ] / mean(weights[1, ])
+      
+      # ratio 2
+      C <- c(beta2 %*% eval_mat[point, ])
+      D <- weights[2, ] / mean(weights[2, ])
+      
+      # Step 1: get lambda search interval from separate ratio bounds
+      bounds1 <- solve_ratio_bounds(A, B, lower_rho, upper_rho)
+      bounds2 <- solve_ratio_bounds(C, D, lower_rho, upper_rho)
+      
+      lambda_lo <- max(bounds1["min"], bounds2["min"])
+      lambda_hi <- min(bounds1["max"], bounds2["max"])
+      
+      lambda_interval[point, "lambda_lo", gg] <- lambda_lo
+      lambda_interval[point, "lambda_hi", gg] <- lambda_hi
+      
+      # no overlap => zero not attainable at this point
+      if (lambda_lo > lambda_hi) {
+        zero_attainable[point, gg] <- FALSE
+        best_lambda[point, gg] <- NA
+        best_slack[point, gg] <- Inf
+        next
+      }
+      
+      # Step 2: adaptive lambda search
+      out <- adaptive_lambda_search(
+        lambda_lo = lambda_lo,
+        lambda_hi = lambda_hi,
+        A = A, B = B, C = C, D = D,
+        lower_rho = lower_rho,
+        upper_rho = upper_rho,
+        tol_slack = 1e-6,
+        grid_init = 15,
+        max_refine = 5
+      )
+      
+      zero_attainable[point, gg] <- out$attainable
+      best_lambda[point, gg] <- out$best_lambda
+      best_slack[point, gg] <- out$best_slack
+      
+      point_attainable[point] <- isTRUE(out$attainable)
+    }
+    
+    # stop early once zero is attainable for at least one point
+    if (all(point_attainable)) {
+      first_gamma_zero_attainable <- this_gamma
+      cat("Stopping early: zero is attainable at gamma =", this_gamma, "\n")
+      break
+    }
+  }
   
+  #------------------------------------------------------------
+  # main gamma loop for the beta
+  #------------------------------------------------------------
+  for (gg in seq_along(gamma_vals)) {
+    
+    this_gamma <- gamma_vals[gg]
+    cat("Start gamma (beta):", this_gamma, "\n")
+    
+    lower_rho <- rep((1 / this_gamma)^lag, time_points)
+    upper_rho <- rep(this_gamma^lag, time_points)
+    
+    point_attainable <- logical(npoints_beta)
+    
+    for (point in seq_len(npoints_beta)) {
+      
+      
+      A <- c(beta1[,point + intercept])
+      
+      B <- weights[1, ] / mean(weights[1, ])
+      
+      C <- c(beta2[,point + intercept])
+      
+      D <- weights[2, ] / mean(weights[2, ])
+      
+      # Step 1: get lambda search interval from separate ratio bounds
+      bounds1 <- solve_ratio_bounds(A, B, lower_rho, upper_rho)
+      bounds2 <- solve_ratio_bounds(C, D, lower_rho, upper_rho)
+      
+      lambda_lo <- max(bounds1["min"], bounds2["min"])
+      lambda_hi <- min(bounds1["max"], bounds2["max"])
+      
+      lambda_interval_beta[point, "lambda_lo", gg] <- lambda_lo
+      lambda_interval_beta[point, "lambda_hi", gg] <- lambda_hi
+      
+      # no overlap => zero not attainable at this point
+      if (lambda_lo > lambda_hi) {
+        zero_attainable_beta[point, gg] <- FALSE
+        best_lambda_beta[point, gg] <- NA
+        best_slack_beta[point, gg] <- Inf
+        next
+      }
+      
+      # Step 2: adaptive lambda search
+      out <- adaptive_lambda_search(
+        lambda_lo = lambda_lo,
+        lambda_hi = lambda_hi,
+        A = A, B = B, C = C, D = D,
+        lower_rho = lower_rho,
+        upper_rho = upper_rho,
+        tol_slack = 1e-6,
+        grid_init = 15,
+        max_refine = 5
+      )
+      
+      zero_attainable_beta[point, gg] <- out$attainable
+      best_lambda_beta[point, gg] <- out$best_lambda
+      best_slack_beta[point, gg] <- out$best_slack
+      
+      point_attainable[point] <- isTRUE(out$attainable)
+    }
+    
+    # stop early once zero is attainable for at least one point
+    if (all(point_attainable)) {
+      first_gamma_zero_attainable_beta <- this_gamma
+      cat("Stopping early: zero is attainable at gamma =", this_gamma, "\n")
+      break
+    }
+  }
+  
+  #------------------------------------------------------------
+  # summary
+  #------------------------------------------------------------
+  find_first_gamma <- function(zero_attainable, gamma_vals) {
+    n_x <- nrow(zero_attainable)
+    
+    pointwise_first_gamma <- sapply(seq_len(n_x), function(i) {
+      idx <- which(zero_attainable[i, ] %in% TRUE)
+      if (length(idx) > 0) gamma_vals[min(idx)] else NA
+    })
+    
+    overall_first_gamma <- {
+      idx <- which(apply(zero_attainable, 2, function(z) any(z %in% TRUE)))
+      if (length(idx) > 0) gamma_vals[min(idx)] else NA
+    }
+    
+    list(
+      overall_first_gamma = overall_first_gamma,
+      pointwise_first_gamma = pointwise_first_gamma
+    )
+  }
+  
+  gamma_summary <- find_first_gamma(zero_attainable, gamma_vals)
+  gamma_summary_beta <- find_first_gamma(zero_attainable_beta, gamma_vals)
+  
+  res <- list(
+    lambda_interval = lambda_interval,
+    best_lambda = best_lambda,
+    best_slack = best_slack,
+    zero_attainable = zero_attainable,
+    first_gamma_zero_attainable = first_gamma_zero_attainable,
+    gamma_summary = gamma_summary,
+    lambda_interval_beta = lambda_interval_beta,
+    best_lambda_beta = best_lambda_beta,
+    best_slack_beta = best_slack_beta,
+    zero_attainable_beta = zero_attainable_beta,
+    first_gamma_zero_attainable_beta = first_gamma_zero_attainable_beta,
+    gamma_summary_beta = gamma_summary_beta,
+    eval_alues = eval_values
+  )
   
   return(res)
   
